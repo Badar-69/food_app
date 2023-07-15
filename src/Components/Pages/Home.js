93703656c0a7bb039aec167eaaf43fb653b76995
Badar-69 <109582705+Badar-69@users.noreply.github.com>
2023-07-16 00:39:27 +0500

    Update Home.js

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import Navbar from '../UI/Navbar'
 import Buttons from '../UI/Buttons'
+import SubForm from '../UI/SubForm'
+import Footer from '../UI/Footer'
+import '../Pages/Home.css'
 import mainImg from '../../assets/main-1.png'
 import smallImg from '../../assets/small-img.jpg'
 import card1 from '../../assets/card-1.png'
@@ -10,7 +13,15 @@ import cafe1 from '../../assets/cafe-1.jpg'
 import cafe2 from '../../assets/cafe-2.jpg'
 import cafe3 from '../../assets/cafe-3.jpg'
 import favImg from '../../assets/fav-img.png'
-import '../Pages/Home.css'
+import custImg from '../../assets/cust-img.png'
+import tomImg from '../../assets/thomas.jpg'
+import partImg1 from '../../assets/part-1.jpg'
+import partImg2 from '../../assets/part-2.jpg'
+import post1 from '../../assets/post-1.jpg'
+import post2 from '../../assets/post-2.jpg'
+import post3 from '../../assets/post-3.jpg'
+import post4 from '../../assets/post-4.jpg'
+
 
 function Home() {
     const cafeData = [
@@ -120,6 +131,8 @@ function Home() {
                 </div>
             </div>
 
+            {/* Restaurant Section */}
+
             <div className="rest-section">
                 <div className="rest-parent">
                     <div className="rest-text">
@@ -143,7 +156,6 @@ function Home() {
                                         <i className="star-icon fa-solid fa-star"></i>
                                         <i className="star-icon fa-solid fa-star"></i>
                                         <i className="star-icon fa-solid fa-star"></i>
-                                        {/* <i className="star-icon fa-solid fa-star"></i> */}
                                         <i className="star-icon fa-solid fa-star-half-stroke"></i>
                                     </div>
 
@@ -165,7 +177,7 @@ function Home() {
 
             </div>
 
-
+            {/* Favourtie Section */}
             <div className="fav-section">
                 <div className="fav-first">
                     <img className='fav-img' src={favImg} alt="" />
@@ -174,10 +186,10 @@ function Home() {
                 <div className="fav-second">
                     <div className="fav-text">
                         <span className="fav-heading">
-                        Food from your favorite restaurants to your table
+                            Food from your favorite restaurants to your table
                         </span>
                         <p className="fav-para">
-                        Pretium lectus quam id leo in vitae turpis massa sed. Lorem donec massa sapien faucibus et molestie. Vitae elementum curabitur vitae nunc.
+                            Pretium lectus quam id leo in vitae turpis massa sed. Lorem donec massa sapien faucibus et molestie. Vitae elementum curabitur vitae nunc.
                         </p>
                     </div>
 
@@ -188,6 +200,182 @@ function Home() {
 
 
             </div>
+
+
+            {/* Services Section */}
+            <div className="serv-section">
+                <div className="serv-text">
+                    <h2 className="serv-heading">Service shows good taste.</h2>
+                </div>
+
+                <div className="serv-block">
+                    <div className="block-text">
+                        <h2 className='block-heading'>976</h2>
+                        <p className='block-para'>Satisfied Customer</p>
+                    </div>
+                </div>
+
+                <div className="serv-block">
+                    <div className="block-text">
+                        <h2 className='block-heading'>12</h2>
+                        <p className='block-para'>Best Restaurants</p>
+                    </div>
+                </div>
+
+                <div className="serv-block">
+                    <div className="block-text">
+                        <h2 className='block-heading'>1K+</h2>
+                        <p className='block-para'>Food Delivered</p>
+                    </div>
+                </div>
+            </div>
+
+            {/* Customer Section */}
+            <div className="cust-section">
+                <div className="cust-first">
+                    <div className="cust-text">
+                        <span className="cust-heading">
+                            What customers say about us
+                        </span>
+                        <p className="cust-para">
+                            "Dapibus ultrices in iaculis nunc sed augue lacus viverra vitae. Mauris a diam maecenas sed enim. Egestas diam in arcu cursus euismod quis. Quam quisque id diam vel".
+                        </p>
+                    </div>
+
+                    <div className="thomas">
+                        <img className='tom-img' src={tomImg} alt="" />
+
+                        <div className="tom-text">
+                            <h6 className='tom-heading'>Thomas Adamson</h6>
+                            <i className="star-icon fa-solid fa-star"></i>
+                            <i className="star-icon fa-solid fa-star"></i>
+                            <i className="star-icon fa-solid fa-star"></i>
+                            <i className="star-icon fa-solid fa-star"></i>
+                            <i className="star-icon fa-solid fa-star-half-stroke"></i>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className="cust-second">
+                    <img className='cust-img' src={custImg} alt="" />
+                </div>
+            </div>
+
+            {/* Partnership section */}
+
+            <div className="part-section">
+                <div className="part-text">
+                    <span className='part-heading'>Want to Join Partnership?</span>
+                </div>
+
+                <div className="part-row">
+                    <div className="part-child part-child-1">
+                        <img className='part-Img' src={partImg1} alt="" />
+
+                        <div className="child-text">
+                            <h3 className='child-heading'>Join Courier</h3 >
+                            <Buttons text='LEARN MORE' className='part-button' />
+                        </div>
+                    </div>
+
+                    <div className="part-child part-child-2">
+                        <img className='part-Img' src={partImg2} alt="" />
+
+                        <div className="child-text">
+                            <h3 className='child-heading'>Join Merchant</h3 >
+                            <Buttons text='LEARN MORE' className='part-button' />
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* News Section */}
+            <div className="news-section">
+                <div className="news-head">
+                    <span className='news-heading'>Latest news and events</span>
+                </div>
+                <div className="news-row">
+                    <div className="post-one">
+                        <img className='post-Img1' src={post1} alt="" />
+
+                        <div className="post-text">
+                            <h3 className='post-heading'>We Have Recieved An Award For The Quality Of Our Work</h3>
+
+                            <p className="post-para">
+                                Donec adipiscing tristique risus nec feugiat in fermentum. Sapien eget mi proin sed libero. Et magnis dis parturient montes nascetur. Praesent semper feugiat nibh sed pulvinar proin gravida.
+                            </p>
+
+                            <a className='post-link' href="/">Read More</a>
+                        </div>
+
+                        <div className="post-credits">
+                            <h6 className='credits-heading'> <i className="post-icons fa-solid fa-user"></i> by QuickEat</h6>
+                            <h6 className='credits-date'> <i className="post-icons fa-solid fa-calendar-days"></i> 01.Jan.2023</h6>
+                            <h6 className='credits-view'> <i className="post-icons fa-solid fa-eye"></i> 132</h6>
+                        </div>
+                    </div>
+
+                    <div className="post-parent">
+                        <div className="post-comn post-two">
+                            <img className='post-comn-img' src={post2} alt="" />
+
+                            <div className="post-info">
+                                <a className='post-tags' href="/">restaurants</a>
+                                <a className='post-tags' href="/">cooking</a>
+
+                                <h5 className='post-info-head'>With Quickeat you can order food for the whole day
+                                </h5>
+
+                                <div className="post-reach">
+                                    <h6 className='credits-heading'> <i className="post-icons fa-solid fa-user"></i> by QuickEat</h6>
+                                    <h6 className='credits-date'> <i className="post-icons fa-solid fa-calendar-days"></i> 01.Jan.2023</h6>
+                                    <h6 className='credits-view'> <i className="post-icons fa-solid fa-eye"></i> 132</h6>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="post-comn post-three">
+                            <img className='post-comn-img' src={post3} alt="" />
+
+                            <div className="post-info">
+                                <a className='post-tags' href="/">restaurants</a>
+                                <a className='post-tags' href="/">cooking</a>
+
+                                <h5 className='post-info-head'>127+ Couriers On Our Team!
+                                </h5>
+
+                                <div className="post-reach">
+                                    <h6 className='credits-heading'> <i className="post-icons fa-solid fa-user"></i> by QuickEat</h6>
+                                    <h6 className='credits-date'> <i className="post-icons fa-solid fa-calendar-days"></i> 01.Jan.2023</h6>
+                                    <h6 className='credits-view'> <i className="post-icons fa-solid fa-eye"></i> 132</h6>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="post-comn post-four">
+                            <img className='post-comn-img' src={post4} alt="" />
+
+                            <div className="post-info">
+                                <a className='post-tags' href="/">restaurants</a>
+                                <a className='post-tags' href="/">cooking</a>
+
+                                <h5 className='post-info-head'>Why You Should Optimize Your Menu for Delivery
+                                </h5>
+
+                                <div className="post-reach">
+                                    <h6 className='credits-heading'> <i className="post-icons fa-solid fa-user"></i> by QuickEat</h6>
+                                    <h6 className='credits-date'> <i className="post-icons fa-solid fa-calendar-days"></i> 01.Jan.2023</h6>
+                                    <h6 className='credits-view'> <i className="post-icons fa-solid fa-eye"></i> 132</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <SubForm/>
+            <Footer/>
 
         </>
     )
