e5bf283d52434da3e3307999356ffeeaa34a89d1
Badar-69 <109582705+Badar-69@users.noreply.github.com>
2023-07-16 00:40:46 +0500

    Created SubForm.js

---
+++ src/Components/UI/SubForm.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import './SubForm.css'
+import subImg from '../../assets/sub-img.png'
+import Buttons from './Buttons'
+
+function SubForm() {
+    return (
+        <>
+            <div className="sub-form">
+                <div className="sub-first">
+                    <img className='sub-Img' src={subImg} alt="Subscribe img" />
+                </div>
+
+                <div className="sub-second">
+                    <div className="sub-text">
+                        <span className='sub-heading'>Get the menu of your favourite restaurants every day</span>
+                        <div className="submit">
+                            <input type="text" id="sub-input" placeholder='Enter email address' />
+                            <Buttons text='SUBSCRIBE' className='sub-button' />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default SubForm
