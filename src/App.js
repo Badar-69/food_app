20e0b66d4bff2082435e5d5b482fce3f178d6fc2
Badar-69 <109582705+Badar-69@users.noreply.github.com>
2023-07-17 02:46:30 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Home from "./Components/Pages/Home";
+import AboutUs from "./Components/Pages/AboutUs";
 
 function App() {
   return (
     <>
       <Home/>
+      <AboutUs/>
     </>
   );
 }
