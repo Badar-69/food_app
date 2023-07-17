dd48c2f52af67b50deccfe8060223e18bb8a956f
Badar-69 <109582705+Badar-69@users.noreply.github.com>
2023-07-18 02:21:26 +0500

    Changes in App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from "./Components/UI/Navbar";
 import Home from "./Components/Pages/Home";
 import AboutUs from "./Components/Pages/AboutUs";
 
 function App() {
   return (
     <>
-      <Home/>
-      <AboutUs/>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<AboutUs />} />
+        </Routes>
+      </Router>
     </>
   );
 }
