25caa5563143db6eac76e149ecc2f50db7225f11
Badar-69 <109582705+Badar-69@users.noreply.github.com>
2023-07-18 02:23:41 +0500

    Changes in Navbar.js

--- src/Components/UI/Navbar.js
+++ src/Components/UI/Navbar.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 import Buttons from './Buttons'
 import logo from '../../assets/logo.png'
 import '../UI/Navbar.css'
@@ -14,11 +15,28 @@ function Navbar() {
                     </div>
 
                     <div className="links">
-                        <a href="/" className="pg-links">Home</a>
+                        {/* <a href="/" className="pg-links">Home</a>
                         <a href="/" className="pg-links">About Us</a>
                         <a href="/" className="pg-links">Restaurants</a>
                         <a href="/" className="pg-links">Pages</a>
-                        <a href="/" className="pg-links">Contacts</a>
+                        <a href="/" className="pg-links">Contacts</a> */}
+
+                        <NavLink exact to="/" className="pg-links" activeClassName="active">
+                            Home
+                        </NavLink>
+                        <NavLink to="/about" className="pg-links" activeClassName="active">
+                            About Us
+                        </NavLink>
+                        <NavLink to="/restaurants" className="pg-links" activeClassName="active">
+                            Restaurants
+                        </NavLink>
+                        <NavLink to="/pages" className="pg-links" activeClassName="active">
+                            Pages
+                        </NavLink>
+                        <NavLink to="/contacts" className="pg-links" activeClassName="active">
+                            Contacts
+                        </NavLink>
+
                     </div>
 
                     <div className="nav-right">
