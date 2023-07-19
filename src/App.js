17c3687d8377416012578204d1aefb4385b18b9a
Badar-69 <109582705+Badar-69@users.noreply.github.com>
2023-07-20 01:50:15 +0500

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import Navbar from "./Components/UI/Navbar";
 import Home from "./Components/Pages/Home";
 import AboutUs from "./Components/Pages/AboutUs";
 import Restaurant from "./Components/Pages/Restaurant";
+import RestCard from "./Components/Pages/RestCard";
 
 function App() {
   return (
     <>
-      <Router>
+      <Router basename="/food_app">
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home />} exact />
           <Route path="/about" element={<AboutUs />} />
           <Route path="/restaurants" element={<Restaurant />} />
+          <Route path="/restaurants/restaurant_card" element={<RestCard />} />
         </Routes>
       </Router>
     </>
