7060facce1be2dec2fba41c23959931c793c559e
Badar-69 <109582705+Badar-69@users.noreply.github.com>
2023-07-25 00:27:17 +0500

    Updated Checkout.js

--- src/Components/Pages/Checkout.js
+++ src/Components/Pages/Checkout.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Checkout.css'
+import Footer from '../UI/Footer'
 import checkImg from '../../assets/cafe-1.jpg'
 import mealImg from '../../assets/dish-4.png'
 import visaImg from '../../assets/visa.png'
@@ -55,43 +56,90 @@ function Checkout() {
                                     </button>
                                 </div>
                             </div>
+                            <hr className='line' />
+
+                            <div className="check-meal">
+                                <img src={mealImg} alt="" className="check-meal-img" />
+                                <h4 className="check-meal-heading">Pasta, kiwi and sauce chilli</h4>
+                                <span className="meal-price">$39</span>
+                            </div>
+
+                            <div className="check-price">
+                                <h3 className="check-price-heading">$39</h3>
+                                <div className="check-quan">
+                                    <button
+                                        className={'dish-btn'}
+                                    >
+                                        -
+                                    </button>
+
+                                    <input type="number" min="0" value='1' readOnly id="dish-input" />
+
+                                    <button className="dish-btn">
+                                        +
+                                    </button>
+                                </div>
+                            </div>
+                            <hr className='line' />
                         </div>
 
                         <div className="check-row-two">
-                            <h4 className="check-info">
-                                <input type="text" placeholder='Full Name' />
-                                <br />
-                                <input type="email" className="check-email"  placeholder='E-mail'/>
-                                <input type="number" className="input-number" placeholder='Phone' />
-                            </h4>
-
-                            <h4 className="address">Delivery addresses</h4>
-                            <select className="check-dropdown">
-                                <option className='drop-option' value="1">California</option>
-                                <option className='drop-option' value="2">California 1</option>
-                                <option className='drop-option' value="3">California 2</option>
-                                <option className='drop-option' value="4">California 3</option>
-                                <option className='drop-option' value="5">California 4</option>
-                            </select>
-
-                            <h4 className="check-pay">Payment Method</h4>
-                            <div className="pay-opt">
-                                <h5 className='pay-card'>Card</h5>
-                                <h5 className='pay-cash'>Cash</h5>
+                            <div className="info-parent">
+                                <div className="check-info">
+                                    <h4 className="info-heading">Buyer Information</h4>
+                                    <input type="text" placeholder='Full Name' className='input-big' />
+                                    <br />
+                                    <input type="email" className="input-small" placeholder='E-mail' />
+                                    <input type="number" className="input-small" placeholder='Phone' />
+                                </div>
                             </div>
 
-                            <img src={visaImg} alt=''  className="visa" />
-                            <img src={masterImg} alt='' className="master" />
-                            <img src={payImg} alt='' className="paypal" />
+                            <div className="address-parent">
+                                <div className="address-info">
+                                    <h4 className="address">Delivery addresses</h4>
+                                    <select className="check-dropdown">
+                                        <option className='drop-option' value="1">California</option>
+                                        <option className='drop-option' value="2">California 1</option>
+                                        <option className='drop-option' value="3">California 2</option>
+                                        <option className='drop-option' value="4">California 3</option>
+                                        <option className='drop-option' value="5">California 4</option>
+                                    </select>
 
-                            <input type="number"  className="card-number" placeholder='Card Number' />
-                            <br />
-                            <input type="number"  className="exp-date"  placeholder='Expiration Date'/>
-                            <input type="number" className="cvv" placeholder='CVV' />
+                                    <input type="text" className="input-big" placeholder='Street' />
+                                    <input type="text" className="input-small" placeholder='House number' />
+                                    <input type="text" className="input-small" placeholder='Apartment number' />
+                                </div>
+                            </div>
+
+                            <div className="pay-parent">
+                                <div className="pay-info">
+                                    <h4 className="check-pay">Payment Method</h4>
+                                    <div className="pay-opt">
+                                        <h5 className='pay pay-card'>Card</h5>
+                                        <h5 className='pay pay-cash'>Cash</h5>
+                                    </div>
+
+                                    <div className="pay-imgs">
+                                        <img src={visaImg} alt='' className="visa" />
+                                        <img src={masterImg} alt='' className="master" />
+                                        <img src={payImg} alt='' className="paypal" />
+                                    </div>
+
+                                    <input type="number" className="input-big" placeholder='Card Number' />
+                                    <br />
+                                    <input type="number" className="input-small" placeholder='Expiration Date' />
+                                    <input type="number" className="input-small" placeholder='CVV' />
+                                </div>
+                            </div>
+
+                            <div className="check-btn">
+                                <button className="btn-send">Send</button>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
+            <Footer />
         </>
     )
 }
