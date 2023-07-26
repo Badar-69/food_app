6d2f1c03192abce3138796a93713e73dd0dd67f5
Badar-69 <109582705+Badar-69@users.noreply.github.com>
2023-07-27 01:01:34 +0500

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { CartProvider } from './Context/Contextcheck';
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <CartProvider>
+      <App />
+    </CartProvider>
+  </React.StrictMode>,
 );
