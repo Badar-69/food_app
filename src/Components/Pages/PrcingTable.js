5cad63d18c522bd978b59cee62d253230627f48e
Badar-69 <109582705+Badar-69@users.noreply.github.com>
2023-08-03 00:15:29 +0500

    Created PricingTable.js

---
+++ src/Components/Pages/PrcingTable.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './PricingTable.css'
+
+function PrcingTable() {
+    return (
+        <>
+            <div className="table-section">
+                <div className="about-us">
+                    <span className='pr-page'>Home <i className="abt-arrow fa-solid fa-arrow-right"></i> <span className="cr-page">Pricing Table</span></span>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default PrcingTable
